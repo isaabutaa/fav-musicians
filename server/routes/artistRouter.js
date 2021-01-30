adf28c31c236542354b6333ff3dd941329a0d22f
Isa Abutaa <isaabutaa@gmail.com>
2021-01-30 12:50:07 -0500

    added Comment schema in models folder, and added a route in ArtistRouter to add a comment in relation to the user and artist

--- server/routes/artistRouter.js
+++ server/routes/artistRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const artist = require('../models/artist.js')
 const artistRouter = express.Router()
 const Artist = require('../models/artist.js')
+const Comment = require('../models/comment.js')
 
 // get all and post one
 artistRouter.route("/")
@@ -15,6 +15,7 @@ artistRouter.route("/")
         })
     })
     .post((req, res, next) => {
+        console.log("posted new artist")
         req.body.user = req.user._id
         const newArtist = new Artist(req.body)
         newArtist.save((err, savedArtist) => {
@@ -69,6 +70,21 @@ artistRouter.route("/:artistId")
             }
         )
     })
+
+// user post comment to an issue
+artistRouter.post("/comments/:artistId", (req, res, next) => {
+    console.log("added comment")
+    req.body.user = req.user._id
+    req.body.artist = req.params.artistId
+    const newComment = new Comment(req.body)
+    newComment.save((err, savedComment) => {
+        if(err) {
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(savedComment)
+    })
+})
 
 
 module.exports = artistRouter
