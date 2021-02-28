c5c73261b13d1e3fb4c1b30e57f0b19c91913d37
Isa Abutaa <isaabutaa@gmail.com>
2021-02-27 19:14:14 -0500

    quick commit

--- server/server.js
+++ server/server.js
@@ -4,26 +4,27 @@ require('dotenv').config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const expressJwt = require('express-jwt')
-const secret = process.env.MY_SECRET
+const mySecret = process.env.MY_SECRET
+const port = process.env.PORT
 
 // middleware
 app.use(express.json())
 app.use(morgan('dev'))
 
 // connect to db
-mongoose.connect("mongodb://localhost:27017/musicians", 
+mongoose.connect("mongodb://localhost:27017/musicians",
     {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
         useFindAndModify: false
     },
-    () => console.log("Connected to musicians database!!!")
+    () => console.log("Connected to the fav-musicians db!!!")
 )
 
 // routes
 app.use("/auth", require("./routes/authRouter.js"))
-app.use("/protected", expressJwt({ secret, algorithms: ['HS256'] }))
+app.use("/protected", expressJwt({ secret: mySecret, algorithms: ['HS256'] }))
 app.use("/protected/artists", require("./routes/artistRouter.js"))
 
 // error handler
@@ -36,4 +37,4 @@ app.use((err, req, res, next) => {
 })
 
 // listen for server
-app.listen(9000, () => console.log("Server is running on Port 9000"))
+app.listen(port, () => console.log(`Server is running on Port ${port}`))
